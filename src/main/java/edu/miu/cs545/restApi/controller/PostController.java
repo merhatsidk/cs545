aacbcf731c1cd778d8daa1d779f3553f8e50ba8e
Merhatsidk <merhatsidk29@gmail.com>
2022-11-01 21:38:12 -0500

    Response status added

--- src/main/java/edu/miu/cs545/restApi/controller/PostController.java
+++ src/main/java/edu/miu/cs545/restApi/controller/PostController.java
@@ -5,6 +5,7 @@ import edu.miu.cs545.restApi.dto.PostRequestDto;
 import edu.miu.cs545.restApi.dto.PostResponseDto;
 import edu.miu.cs545.restApi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,21 +18,25 @@ public class PostController {
     private PostService postService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<PostResponseDto> getPosts(){
         return postService.findAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public PostResponseDto getById(@PathVariable("id") long id){
         return postService.findById(id);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody PostRequestDto postRequestDto){
         postService.save(postRequestDto);
     }
 
     @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete( long id){
         postService.deleteById(id);
     }
